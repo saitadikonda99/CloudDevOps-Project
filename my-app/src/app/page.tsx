08a0c165e8f344c2403d45d16b7aee06f11bc8ab
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-12-03 19:00:38 +0530

    Next.js basic app created

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import './page.css';
+
+export default function Home() {
+  return (
+      <div className="HomeComponent">
+        <div className="HomeComponent-in">
+          <h1>CloudDevOps Project</h1>
+        </div>
+      </div>
+  );
+}
